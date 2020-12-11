dbcf484e75889cac9eb74570de37ca387ccefa11
Ramakrishna Basanthapuram <rkreddy56@gmail.com>
2020-12-11 07:49:13 -0800

    git new

--- src/app/crypto-home/crypto-home.component.ts
+++ src/app/crypto-home/crypto-home.component.ts
@@ -10,6 +10,11 @@ import { AngularFireDatabase } from '@angular/fire/database';
 export class CryptoHomeComponent implements OnInit {
   cryptoData: any = [];
   items: any;
+  ETH : any;
+  BTC : any;
+  IETH : any;
+  IBTC : any;
+  counter = 0;
 
   constructor(private cryptoService : CryptoService, private db : AngularFireDatabase) { }
 
@@ -21,13 +26,35 @@ export class CryptoHomeComponent implements OnInit {
   }
 
   getData() {
+    setInterval(()=> {
+      this.counter = 0;
+    },30000)
     this.cryptoService.getCryptoDetails().subscribe(data => {
       this.cryptoData = data;
+      if(this.counter === 0){
+        this.IBTC = this.cryptoData[0]['price'];
+        this.IETH = this.cryptoData[0]['price'];
+      }
+      this.BTC = this.cryptoData[0]['price'];
+      this.ETH = this.cryptoData[0]['price'];
+      if(this.ETH < this.IETH) {
+        if((this.IETH - this.ETH > 5 && this.IETH - this.ETH < 10)) {
+          this.sendUpdate(this.ETH,'5-10')
+        }else if((this.IETH - this.ETH > 10 && this.IETH - this.ETH < 15)) {
+          this.sendUpdate(this.ETH,'10-15')
+        }
+      }else {
+        if((this.ETH - this.IETH > 5 && this.ETH - this.IETH < 10)) {
+          this.sendUpdate(this.ETH,'5-10')
+        }else if((this.ETH - this.IETH > 10 && this.ETH - this.IETH < 15)) {
+          this.sendUpdate(this.ETH,'10-15')
+        }
+      }
     }) 
   }
 
-  sendUpdate() {
-    this.items.update({value : '656565'});
+  sendUpdate(value: any,diff: string) {
+    this.items.update({value : value, difference : diff });
   }
 
 }
